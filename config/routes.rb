21d7385d86733e2a5ba40a12158147d32bab5e53
yusei.baba <yusei.programming@gmail.com>
2021-03-06 17:23:51 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#       Prefix Verb   URI Pattern                  Controller#Action
+#     messages GET    /messages(.:format)          messages#index
+#              POST   /messages(.:format)          messages#create
+#  new_message GET    /messages/new(.:format)      messages#new
+# edit_message GET    /messages/:id/edit(.:format) messages#edit
+#      message GET    /messages/:id(.:format)      messages#show
+#              PATCH  /messages/:id(.:format)      messages#update
+#              PUT    /messages/:id(.:format)      messages#update
+#              DELETE /messages/:id(.:format)      messages#destroy
+ root to: 'messages#index'
+
+ resources :messages
+
 end
